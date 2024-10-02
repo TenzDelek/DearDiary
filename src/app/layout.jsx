2b57b5a3520cbda4724e8efbfb86739537d31b08
Ananya <ananyask.btech22@rvu.edu.in>
2024-10-03 02:17:11 +0530

    Added Favicon

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -15,6 +15,11 @@ const geistMono = localFont({
 export const metadata = {
   title: "DearDiary",
   description: "A Digital Diary for the future",
+  icons: {
+    icon: "/favicon/favicon?v=4.ico",
+    apple:"/favicon/favicon/apple-touch-icon.png?v=4",
+    shortcut:"/favicon/favicon/apple-touch-icon.png"
+  },
 };
 
 export default function RootLayout({ children }) {
