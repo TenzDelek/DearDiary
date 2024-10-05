5a55686e3c879bf5f23082994ec3a0a5dc07fd74
mchen610 <melvinchen610@gmail.com>
2024-10-04 23:55:15 -0400

    change to spinner

--- src/app/guestbook/form.jsx
+++ src/app/guestbook/form.jsx
@@ -55,11 +55,15 @@ export default function Form({ onNewEntry }) {
         className="pl-4 pr-32 py-2 mt-1 focus:ring-blue-500 focus:border-blue-500 block w-full border-neutral-300 rounded-md bg-gray-100 dark:bg-neutral-800 text-neutral-900 dark:text-neutral-100"
       />
       <button
-        className="flex items-center justify-center absolute right-1 top-1 px-2 py-1 font-medium h-8 bg-neutral-200 dark:bg-neutral-700 text-neutral-900 dark:text-neutral-100 rounded w-16"
+        className="flex items-center justify-center absolute right-1 top-1 px-2 py-1 font-medium h-8 bg-neutral-200 dark:bg-neutral-800 text-neutral-900 dark:text-neutral-100 rounded w-16"
         disabled={pending}
         type="submit"
       >
-        {pending ? "Signing..." : "Sign"}
+        {pending ? (
+          <div className="spinner border-t-transparent border-4 border-neutral-400 rounded-full w-4 h-4 animate-spin"></div>
+        ) : (
+          "Sign"
+        )}
       </button>
       {error && <p className="text-red-500">{error}</p>}
     </form>
