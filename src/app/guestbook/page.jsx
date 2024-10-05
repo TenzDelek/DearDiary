c41b03ec9b61f15c7ffd15d63bba1acfd91d64f9
mchen610 <melvinchen610@gmail.com>
2024-10-04 23:37:00 -0400

    fix text on guestbook

--- src/app/guestbook/page.jsx
+++ src/app/guestbook/page.jsx
@@ -57,13 +57,13 @@ function Guestbook() {
     <main className="flex min-h-screen flex-col items-center p-12">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Giridhar&apos;s Guestbook
+          Our Guestbook
         </p>
       </div>
 
       <div className="relative w-full max-w-2xl my-12">
         <h1 className="relative font-semibold dark:drop-shadow-[0_0_0.3rem_#ffffff70] text-2xl mb-8 tracking-tighter">
-          Sign my guestbook
+          Sign our guestbook!
         </h1>
         <Suspense fallback={<p>Loading form...</p>}>
           <GuestbookForm />
