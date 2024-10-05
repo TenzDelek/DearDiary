8dc46929f506355e98ea7bf0aa2e21dac77c8866
lade6501 <lade6501@gmail.com>
2024-10-05 14:26:29 +0530

    feat: Added smooth scrolling using lenis

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,24 @@
+"use client";
+
+import { useEffect } from "react";
+import Lenis from "lenis";
 import Image from "next/image";
 
 export default function Home() {
+  useEffect(() => {
+    const lenis = new Lenis();
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-       <p>DearDiary: A Digital Note in this fast moving world</p>
-       </main>
-      
+        <p>DearDiary: A Digital Note in this fast moving world</p>
+      </main>
     </div>
   );
 }
