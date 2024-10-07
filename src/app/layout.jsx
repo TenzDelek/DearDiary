e05f807da0497bf9e9957cccf815e89fa017f2b3
Shivam <143623476+myselfshivams@users.noreply.github.com>
2024-10-07 11:26:36 +0530

    Added:: Meta Tags, robots.txt, sitemap for SEO Enhancement

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,6 +2,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
 import Navbar from '../components/Navbar';
+import Head from 'next/head';
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -27,8 +28,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
+      <Head>
+        <title>DearDiary - A Digital Diary for the future</title>
+        <meta name="description" content="A Digital Diary to capture and share your personal moments." />
+        <meta name="keywords" content="digital diary, personal blog, online journal" />
+        <meta property="og:title" content="DearDiary - A Digital Diary for the future" />
+        <meta property="og:description" content="A Digital Diary to capture and share your personal moments." />
+        <meta property="og:image" content="/path_to_your_image.jpg" />
+        <meta property="og:url" content="https://dear-diary-black.vercel.app/" />
+        <meta property="og:type" content="website" />
+        <meta name="robots" content="index, follow" />
+        <link rel="icon" href="/favicon/favicon/favicon.ico?v=4" />
+        <link rel="apple-touch-icon" href="/favicon/favicon/apple-touch-icon.png?v=4" />
+      </Head>
       <html lang="en" className="dark">
-        <body
+      <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
           <header className="flex items-center justify-between p-2  text-white">
