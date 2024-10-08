2e2f2758487421fbd8b265026968d81ca908950e
Sourabh782 <sourabhsinghrawat782@gmail.com>
2024-10-08 20:19:47 +0530

    improved navbar interaction

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,11 +3,26 @@ import Link from 'next/link';
 export default function Navbar() {
   return (
     <nav className="flex justify-center space-x-8 p-4  text-white">
-      <Link href="/">Home</Link>
-      <Link href="/notes">Notes</Link>
-      <Link href="/daily-quote">Daily Quote</Link>
-      <Link href="/guestbook">Guestbook</Link>
-      <Link href="/about">About</Link>
+      <div className='py-2 group'>
+        <Link href="/">Home</Link>
+        <div className='w-0 group-hover:w-full h-[2px] rounded-full bg-white transition-all delay-750'></div>
+      </div>
+      <div className='py-2 group'>
+        <Link href="/notes">Notes</Link>
+        <div className='w-0 group-hover:w-full h-[2px] rounded-full bg-white transition-all delay-750'></div>
+      </div>
+      <div className='py-2 group'>
+        <Link href="/daily-quote">Daily Quote</Link>
+        <div className='w-0 group-hover:w-full h-[2px] rounded-full bg-white transition-all delay-750'></div>
+      </div>
+      <div className='py-2 group'>
+        <Link href="/guestbook">Guestbook</Link>
+        <div className='w-0 group-hover:w-full h-[2px] rounded-full bg-white transition-all delay-750'></div>
+      </div>
+      <div className='py-2 group'>
+        <Link href="/about">About</Link>
+        <div className='w-0 group-hover:w-full h-[2px] rounded-full bg-white transition-all delay-750'></div>
+      </div>
     </nav>
   );
 }
