a4dc920d91bd78626d7d7478d367f622012c156c
Sourabh782 <sourabhsinghrawat782@gmail.com>
2024-10-08 20:51:04 +0530

    implemented code reusablity

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,25 @@
 import Link from 'next/link';
 
+const items = [
+  {link:"/", name: "Home"},
+  {link:"/notes", name: "Notes"},
+  {link:"/daily-quote", name: "Daily Quote"},
+  {link:"/guestbook", name: "Guestbook"},
+  {link:"/about", name: "About"},
+]
+
 export default function Navbar() {
   return (
     <nav className="flex justify-center space-x-8 p-4  text-white">
-      <div className='py-2 group'>
-        <Link href="/">Home</Link>
-        <div className='w-0 group-hover:w-full h-[2px] rounded-full bg-white transition-all delay-750'></div>
-      </div>
-      <div className='py-2 group'>
-        <Link href="/notes">Notes</Link>
-        <div className='w-0 group-hover:w-full h-[2px] rounded-full bg-white transition-all delay-750'></div>
-      </div>
-      <div className='py-2 group'>
-        <Link href="/daily-quote">Daily Quote</Link>
-        <div className='w-0 group-hover:w-full h-[2px] rounded-full bg-white transition-all delay-750'></div>
-      </div>
-      <div className='py-2 group'>
-        <Link href="/guestbook">Guestbook</Link>
-        <div className='w-0 group-hover:w-full h-[2px] rounded-full bg-white transition-all delay-750'></div>
-      </div>
-      <div className='py-2 group'>
-        <Link href="/about">About</Link>
-        <div className='w-0 group-hover:w-full h-[2px] rounded-full bg-white transition-all delay-750'></div>
-      </div>
+      {
+        items.map((item, index)=>(
+          <div key={index} className='py-2 group'>
+            <Link href={item.link}> {item.name} </Link>
+            <div className='w-0 group-hover:w-full h-[2px] rounded-full bg-white transition-all delay-750'></div>
+          </div>
+        ))
+      }
+      
     </nav>
   );
 }
