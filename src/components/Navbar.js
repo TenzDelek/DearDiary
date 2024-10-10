62a88dd635343813d8d4651cd144dfd588932af0
Sourabh782 <sourabhsinghrawat782@gmail.com>
2024-10-10 10:09:45 +0530

    added navbar responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,7 @@
+"use client"
+import { CircleX, Menu } from 'lucide-react';
 import Link from 'next/link';
+import { useState } from 'react';
 
 const items = [
   {link:"/", name: "Home"},
@@ -9,17 +12,28 @@ const items = [
 ]
 
 export default function Navbar() {
+  const [open, setOpen] = useState(true)
   return (
-    <nav className="flex justify-center space-x-8 p-4  text-white">
+    <>
+      <button className='sm:hidden z-10' onClick={()=>setOpen(!open)}>
+
+        { !open ? <Menu /> :
+        <CircleX />}
+      </button>
       {
-        items.map((item, index)=>(
-          <div key={index} className='py-2 group'>
-            <Link href={item.link}> {item.name} </Link>
-            <div className='w-0 group-hover:w-full h-[2px] rounded-full bg-white transition-all delay-750'></div>
-          </div>
-        ))
+        // open &&
+        <nav className={`flex-col absolute top-0 left-0 items-center h-screen sm:h-auto ${open ? 'w-full' : 'hidden'} sm:flex sm:w-auto bg-[#171717] sm:bg-transparent z-[2] sm:relative sm:flex-row flex gap-8  justify-center sm:space-x-8 p-4  text-white`}>
+        {
+          items.map((item, index)=>(
+            <div key={index} className='py-2 group'>
+              <Link href={item.link} onClick={()=>setOpen(false)}> {item.name} </Link>
+              <div className='w-0 group-hover:w-full h-[2px] rounded-full bg-white transition-all delay-750'></div>
+            </div>
+          ))
+        }
+        
+      </nav>
       }
-      
-    </nav>
+    </>
   );
 }
